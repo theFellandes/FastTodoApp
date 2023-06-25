bb94d6cd6e5fc3ac794f5d8f02e913428bf5c2f7
theFellandes <57584110+theFellandes@users.noreply.github.com>
2023-06-25 15:46:00 +0300

    Add postgre sql connector

--- connectors/solid/postgre_sql_connector.py
+++ connectors/solid/postgre_sql_connector.py
@@ -1,19 +1,29 @@
+import psycopg2
+
 from connectors.abstract.sql_connector import SQLConnector
 
 
 class PostgreSqlConnector(SQLConnector):
 
     def connect(self):
-        ...
+        self._connection: psycopg2.connection = psycopg2.connect(
+            host=self._host,
+            port=self._port,
+            database=self._database,
+            user=self._user,
+            password=self._password
+        )
+        self._cursor: psycopg2.cursor = self._connection.cursor()
 
     def disconnect(self):
-        ...
+        self._cursor.close()
+        self._connection.close()
 
     def execute(self, query: str):
-        ...
+        self._cursor.execute(query)
 
     def fetch(self, query: str):
-        ...
+        return self._cursor.fetchall()
 
     def commit(self):
-        ...
+        self._connection.commit()
